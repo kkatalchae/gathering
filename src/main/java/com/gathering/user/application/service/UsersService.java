8e43b2fc5c1bebd4fe0c319c56ff229c17127b7b
kkatalyst <byeongchae44@gmail.com>
2025-08-20 17:50:22 +0900

    feat: 비밀번호 암복호화 및 스프링 시큐리티 적용

--- src/main/java/com/gathering/user/application/service/UsersService.java
+++ src/main/java/com/gathering/user/application/service/UsersService.java
@@ -1,11 +1,17 @@
 package com.gathering.user.application.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.gathering.user.domain.model.UserSecurityEntity;
+import com.gathering.user.domain.model.UsersEntity;
 import com.gathering.user.domain.repository.UserSecurityRespository;
 import com.gathering.user.domain.repository.UsersRepository;
 import com.gathering.user.presentation.dto.UserJoinRequest;
+import com.gathering.util.CryptoUtil;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -14,11 +20,21 @@ public class UsersService {
 
 	private final UsersRepository usersRepository;
 	private final UserSecurityRespository userSecurityRespository;
+	private final PasswordEncoder passwordEncoder;
 
+	@Value("${crypto.aes.key}")
+	private String aesKey;
+
+	private String getPasswordHash(String encryptedPassword) {
+		String decryptedPassword = CryptoUtil.decryptAES(encryptedPassword, aesKey);
+		return passwordEncoder.encode(decryptedPassword);
+	}
+
+	@Transactional
 	public void join(UserJoinRequest request) {
-
-		usersRepository.save(UserJoinRequest.toUsersEntity(request));
-		userSecurityRespository.save(UserJoinRequest.toUserSecurityEntity(request));
-
+		UsersEntity usersEntity = usersRepository.save(UserJoinRequest.toUsersEntity(request));
+		UserSecurityEntity userSecurityEntity = UserSecurityEntity.of(usersEntity.getTsid(),
+			getPasswordHash(request.getPassword()));
+		userSecurityRespository.save(userSecurityEntity);
 	}
 }
